3a307cd1e9730fb20e21b784862c5fed9b1eee74
lollith <adeline.gouet@gmail.com>
2022-02-25 18:26:45 +0000

    5 a finir

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
@@ -34,7 +34,7 @@ void bubble_sort( int *tab, int size);
 int *ft_indexing(int *input, int *stack_a, int size);
 
 int *ft_tab_ft(int k, int *stack_a);
-void    ft_3_sort (int *stack_a);
+void    ft_3_sort (int *stack_a, int *save);
 void    ft_3_sort_print(int i,int j, int e);
 int	find_max(int *stack, int size);
 int	ft_nbr_unit_bin(int nbr);
--- sources/push_swap_main.c
+++ sources/push_swap_main.c
@@ -11,12 +11,11 @@
 /* ************************************************************************** */
 #include <stdio.h>  //printf
 #include "push_swap.h"
-void ft_putchar(int a )
+
+void	ft_putchar(int a )
 {
-	write(1,&a ,1);
+	write (1, &a, 1);
 }
-
-
 
 // libft a ajouter/ atoi a supprimer
 int	ft_atoi(const char *nptr)
@@ -45,71 +44,37 @@ int	ft_atoi(const char *nptr)
 	return (result);
 }
 
-
-int	main (int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-	int stack_a[argc -1];
-	int stack_b[argc - 1];
-	int input[argc - 1];
-	int i;
-	int j;
+	int	stack_a[argc - 1];
+	int	stack_b[argc - 1];
+	int	input[argc - 1];
+	int	i;
+	int	save[3];
 
 	i = 1;
 	if (argc < 2 )
 		return (0);
-	while(argv[i] && i < argc) // recup arg ds input
+	while(argv[i] && i < argc)// recup arg ds input
 	{
 		input[i - 1] = ft_atoi(argv[i]);
 		i++;
 	}
-    if ((!check_order(input, argc - 1))) 
-        return 0;
-    else
-        ft_indexing(input, stack_a, argc - 1);
-    
-    if (argc == 3) // 2 chiffres		
+	if ((!check_order(input, argc - 1)))
+		return (0);
+	else
+		ft_indexing(input, stack_a, argc - 1);
+	if (argc == 3) // 2 chiffres		
 		write(1, "sa\n", 3);
-  
-    if (argc == 4) // 3 chiffres	
-        ft_3_sort(stack_a);
-
-//apres 4 chiffres
-    int input_order[argc - 1];	
-	if (argc >= 5)
+	if (argc == 4) // 3 chiffres	
 	{
+		ft_memcpy(stack_a, save, sizeof(int) * 3);
+		ft_3_sort(stack_a, save);
+	}
+	//4 et 5 chiffres
+	//if ((argc == 5)||(argc == 6)
 
-	    ft_memcpy (stack_a, input, sizeof(int) * (argc - 1));
-		if ((!check_order(stack_a, argc -1))) 
-        {   
-            return 0;
-        }
-        else
-        {
-            ft_memcpy (input_order, input, sizeof(int) * (argc - 1));
-            bubble_sort (input_order, argc - 1);
-        
-            i = 0;
-            while(i < argc - 1)
-            {
-                j= 0;
-                while (j < argc - 1)
-                {
-                    if (input[i] == input_order [j])
-                        stack_a[i] = j;
-                    j++;
-                }   
-                i++;
-            }
 
-            /*     printf("stacka %d\n", stack_a[0]);
-                 printf("stacka %d\n", stack_a[1]);
-                 printf("stacka %d\n", stack_a[2]);
-                printf("stacka %d\n", stack_a[3]);
-                printf("stacka %d\n", stack_a[4]);*/
-
-        		ft_radix(stack_a, stack_b, argc - 1);
-        }   
-    }
-
-	return (0);
+	if (argc >= 7)//6 chiffres
+		ft_radix(stack_a, stack_b, argc - 1);
 }
